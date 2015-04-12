7aca10b73692071046aaf0c3d755e36fd1e03428
mcorrea <manuel.correa@careerbuilder.com>
2015-04-12 14:20:26 -0400

    fixing test and 3.0 format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,16 @@
+# make sure we have the correct gems to get off the ground
+require 'rubygems'
+require 'bundler'
+require 'sinatra-scaffolder'
+
+Bundler.require(:default, :test) if defined?(Bundler)
+
+# set up the environment explicitly
+ENV['RACK_ENV'] = 'test'
+
+# code coverage
 if ENV['COVERAGE']
   require 'simplecov'
   SimpleCov.start
 end
 
-require 'sinatra-scaffolder'
-
-ENV['environment'] = 'test'
