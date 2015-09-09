b584c43a9c41ada122735e1fa1b9383864b85e6a
Denton Greenfield <denton.greenfield@careerbuilder.com>
2015-09-09 15:58:56 -0400

    The usage given by the CLI itself was technically wrong since it fails to demonstrate that you need a project name argument

--- lib/sinatra-scaffolder/cli.rb
+++ lib/sinatra-scaffolder/cli.rb
@@ -3,7 +3,7 @@ require 'sinatra-scaffolder/create'
 
 module SinatraScaffolder
   class CLI < Thor
-    desc 'create', 'sets up all of the files and directories necessary for Sinatra development'
+    desc 'create [PROJECT_NAME]', 'sets up all of the files and directories necessary for Sinatra development'
     def create(project_name)
       SinatraScaffolder::Create.create project_name
     end
